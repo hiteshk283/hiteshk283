1b95862ddd9bd79fca256bfc8e6c930f60ef4457
Hitesh Kumar <kumarh5149@GMAIL.com>
2025-06-07 11:52:12 +0530

    Installing NPM

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
-import IndexController from '../controllers/index';
+import { IndexController } from '../controllers/index';
 
 const router = Router();
-const indexController = new IndexController();
+const indexController: IIndexController = new IndexController();
 
-export function setRoutes(app) {
+interface App {
+    use(path: string, handler: Router): void;
+}
+
+interface IIndexController {
+    getFlavors(req: Express.Request, res: Express.Response): void;
+    getOrders(req: Express.Request, res: Express.Response): void;
+}
+
+export function setRoutes(app: App): void {
     app.use('/flavors', router.get('/', indexController.getFlavors.bind(indexController)));
     app.use('/orders', router.get('/', indexController.getOrders.bind(indexController)));
 }
